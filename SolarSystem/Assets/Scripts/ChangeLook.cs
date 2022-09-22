d82a53ec0358111c001adc4c0ed66ad005a370c3
Iheb Mbarki <45261121+ihebmbarki@users.noreply.github.com>
2022-09-22 18:01:39 +0100

    initial commit

---
+++ SolarSystem/Assets/Scripts/ChangeLook.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLook : MonoBehaviour
+{
+    public GameObject target;
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        lookAt.target = target;
+        Camera.main.fieldOfView = Mathf.Clamp(60 * target.transform.localScale.x, 1, 100);
+    }
+}
